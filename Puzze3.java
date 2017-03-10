83127777ec3ede98dae882bf465dc4744a78633b
agordov <agordov96@gmail.com>
2017-03-10 20:49:42 +0300

    Add files via upload
    Added generics, fixed mistakes

--- Puzze3.java
+++ Puzze3.java
@@ -3,30 +3,37 @@ package homework;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by aleksey on 06.03.17.
- */
 public class Puzze3 {
-    public static final int n = 10;
-    public static void main(String[] args) {
-        int i;
-        List<Integer> list1 = new ArrayList<Integer>();
-        for (i = 0; i < n; i++)
-            list1.add(i);
-        printList(list1);
-        List<Integer> list2 = new ArrayList<Integer>();
-        for (i = 0; i < n/2; i++) {
-            list2.add(2*i, list1.get(i));
-            list2.add(2*i+1, list1.get(n-i-1));
+    private static final int n = 9;
+
+    public static <T> void printList(List<T> inputArr) {
+        for (T elem : inputArr) {
+            System.out.print(elem + " ");
         }
         System.out.println();
-        printList(list2);
     }
 
-    public static void printList(List<Integer> list1) {
-        for (Integer e : list1) {
-            System.out.print(e + " ");
+    public static <T> List<T> transformList(List<T> listIn) {
+        List<T> listOut = new ArrayList<>();
+        for (int i = 0, j = 2; i < listIn.size()-1; i++) {
+            if (i % 2 == 0) {
+                listOut.add(listIn.get(i/2));
+            }
+            else {
+                listOut.add(listIn.get(listIn.size()- j++));
+            }
         }
+        listOut.add(listIn.get(listIn.size()-1));
+        return listOut;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> listIn = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            listIn.add(i);
+        }
+        printList(listIn);
+        printList(transformList(listIn));
     }
 }
 
