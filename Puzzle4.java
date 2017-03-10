857d0bf9e0da2e3599cd1cc68c62b86e1dd67487
agordov <agordov96@gmail.com>
2017-03-10 20:54:10 +0300

    Update Puzzle4.java

--- Puzzle4.java
+++ Puzzle4.java
@@ -1,16 +1,12 @@
 package homework;
 import java.lang.*;
-/**
- * Created by aleksey on 06.03.17.
- */
 public class Puzzle4 {
     public static void main(String[] args) {
         printList(6);
     }
 
     public static void printList(int n) {
-        int i;
-        for (i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             if (i % 2 == 0)
                 reversePrint(i);
             else
@@ -19,16 +15,14 @@ public class Puzzle4 {
     }
 
     public static void reversePrint(int n) {
-        int i;
-        for (i = n; i > 0; i--) {
+        for (int i = n; i > 0; i--) {
             System.out.print(i == 1 ? i : i + "-");
         }
         System.out.println();
     }
 
     public static void straightPrint(int n) {
-        int i;
-        for (i = 1 ; i <= n; i++) {
+        for (int i = 1 ; i <= n; i++) {
             System.out.print(i == n ? i : i + "-");
         }
         System.out.println();
