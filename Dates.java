7c60652a56536f7333424dc720b468df585feb26
agordov <agordov96@gmail.com>
2017-03-15 18:26:10 +0300

    One line was excessive

--- Dates.java
+++ Dates.java
@@ -7,7 +7,6 @@ import java.util.Calendar;
 
 public class Dates{
     public static void howMuchTime(LocalDate birthDay){
-        Calendar cal = Calendar.getInstance();
         LocalDate today = LocalDate.now();
         Period p = Period.between(birthDay, today);
         System.out.println("You are " + p.getYears() + " years " + p.getMonths() + " months " + p.getDays() + " days old");
