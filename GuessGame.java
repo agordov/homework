a196d3c2649a4032b0ef0e49ac51fda97ca08921
agordov <agordov96@gmail.com>
2017-03-19 20:29:29 +0300

    Update GuessGame.java
    Added resource try, "if" logic changed

--- GuessGame.java
+++ GuessGame.java
@@ -12,34 +12,33 @@ public class GuessGame{
     }
 
     public static void guessGame() {
-        try {
-            boolean flag = false;
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
             char in;
             String inputLine;
             char randChar = randomNumber('A', 'Z');
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-            do {
+            while (true) {
                 inputLine = bufferedReader.readLine();
                 if (inputLine.isEmpty()) {
                     System.out.println("String can't be empty");
-                }
-                else {
+                } else {
                     in = inputLine.charAt(0);
-                    if (in >= 'A' && in <= 'Z' && in == randChar) {
-                        flag = true;
-                    }
-                    else if (in >= 'A' && in < randChar) {
-                        System.out.println("Larger!");
-                    }
-                    else if (in <= 'Z' && in > randChar) {
-                        System.out.println("Less!");
+                    if (in >= 'A' && in <= 'Z') {
+                        if (randChar < in) {
+                            System.out.println("Less!");
+                        } else if (randChar > in) {
+                            System.out.println("Larger!");
+                        } else {
+                            System.out.println("Success!");
+                            break;
+                        }
+                    } else {
+                        System.out.println("Your symbol is out of borders");
                     }
                 }
-            } while (!flag);
-            System.out.println("Success!");
+            }
         }
         catch (IOException err) {
-            System.err.println("Caught IOException " + err.getMessage());
+            err.printStackTrace();
         }
     }
     public static void main(String[] args){
