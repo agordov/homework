c28e51eb4b39db8a63fd20522602c8739fd60bc8
agordov <agordov96@gmail.com>
2017-03-19 20:30:30 +0300

    Update RandomNumber.java
    Changed logic, some methods added

--- RandomNumber.java
+++ RandomNumber.java
@@ -6,22 +6,31 @@ public class RandomNumber {
 
 //    return highest digit of random three-digit number
 
-    public static int randomNumber(int lowerN, int higherN) {
-        return (new Random()).nextInt((higherN - lowerN) + 1) + lowerN;
+    public static int randomNumber(int lowerN, int upperN) {
+        if (lowerN > upperN) {
+            int tmp = lowerN;
+            lowerN = upperN;
+            upperN = tmp;
+        }
+        return (new Random()).nextInt((upperN - lowerN) + 1) + lowerN;
     }
 
-    public static int randomNumberMax(int lowerN, int higherN) {
-        int randN = randomNumber(lowerN, higherN);
-        System.out.println(randN);
-        int a = randN % 10;
-        randN /= 10;
-        int b = randN % 10;
-        randN /= 10;
-        int c = randN % 10;
-        return (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+    public static char maxOfTwo(char a, char b) {
+        return a > b ? a : b;
+    }
+
+    public static char maxDigit(int num) {
+        char[] numArr = ("" + num).toCharArray();
+        char max = numArr[0];
+        for (int i = 1; i < numArr.length; i++) {
+            max = maxOfTwo(max, numArr[i]);
+        }
+        return max;
     }
 
     public static void main(String... args) {
-        System.out.println(randomNumberMax(100, 999));
+        int randN = randomNumber(100, 999);
+        System.out.println(randN);
+        System.out.println(maxDigit(randN));
     }
 }
