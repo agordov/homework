41075306b7d24cefef980f45b5f83872cd86a747
agordov <agordov96@gmail.com>
2017-03-22 21:10:00 +0300

    Update ApartmentHouse.java

--- City/ApartmentHouse.java
+++ City/ApartmentHouse.java
@@ -1,7 +1,31 @@
 package homework.City;
 
-/**
- * Created by aleksey on 22.03.17.
- */
-public class ApartmentHouse {
+public class ApartmentHouse extends House{
+
+    int flatnum;
+    int[] flats;
+
+    public ApartmentHouse(String street, int num, int floornum, int flatnum) {
+        super.street = street;
+        setNum(num);
+        if (flatnum > 0) {
+            this.flatnum = flatnum;
+            flats = new int[flatnum];
+        }
+        else
+            throw new IllegalArgumentException();
+    }
+
+    public void setInhabInFlat(int flatnum, int inhabnum) {
+        if (flatnum > 0 && flatnum <= flats.length && inhabnum > 0) {
+            flats[flatnum] = inhabnum;
+            super.inhabnum += inhabnum;
+        }
+        else
+            throw new IllegalArgumentException();
+    }
+
+    public int getFlatnum() {
+        return flatnum;
+    }
 }
